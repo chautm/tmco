4d1788216c23200fd977fa27585ea66ea91b7a20
chautm <chautm@gmail.com>
2016-08-06 08:22:55 -0400

    update phone

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
     <address>               
 				<br/>
-               <p>312-4800 Boulevard Decarie, Montreal, QC  H3X 2H5 &bull;               
+               <p>312-4800 Boulevard Decarie, Montreal, QC  H3X 2H5               
 			   </p>
                <?php 
 				echo "Copyright &copy; 2015 - " . date("Y") ;
